9f32842cba17a80e6933c0f4f76564b2b88d1f4e
Willem Green <willberforce15@gmail.com>
2021-01-07 15:44:09 -0600

    second commit

---
+++ seeds/seed.brew_db.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+TRUNCATE
+    brew_brews
+    brew_users
+    RESTART IDENTITY CASCADE;
+
+INSERT INTO brew_users (user_name, password)
+VALUES 
+    ('testuser1', 'password'),
+    ('testuser2', 'example'),
+    ('testuser3', 'testingpassword');
+
+INSERT INTO brew_brews (name, description, method, input, output, brew_time, grind, roast_level, user_id)
+VALUES
+    ('test title 1', 'this is the first test description', 'kalita', '36', '596', '3:00', 'medium', 'light/medium', 1),
+    ('test title 2', 'this is the second test description', 'V60', '21', '380', '2:30', 'medium', 'light', 2),
+    ('test title 3', 'this is the third test description', 'automatic', '36', '596', '5:00', 'medium/coarse', 'medium', 3);
+
+COMMIT;
